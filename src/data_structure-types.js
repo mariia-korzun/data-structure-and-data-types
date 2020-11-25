f32e8f0f9b2e6836b64326898b429ebe9724d214
Mariia Korzun <3151020m@gmail.com>
2020-11-26 00:41:34 +0200

    Data structure and data types task is done.

--- src/data_structure-types.js
+++ src/data_structure-types.js
@@ -1,11 +1,22 @@
 const validateTitle = (value) => {
-  // Student implementation
-  return;
+  if (typeof value !== 'string') { return 'Incorrect input data' }
+  if (value[0].localeCompare('A') < 0 || value[0].localeCompare('Z') > 0 || value.length <= 2 || value.length >= 20) { return 'INVALID' }
+  return 'VALID'
+
 }
 
 const sum = (value1, value2) => {
-  // Student implementation
-  return;
+  let string
+  let number
+  if (typeof value1 === 'string') {
+    string = value1
+    number = value2
+  } else {
+    string = value2
+    number = value1
+  }
+  if (number % 15 === 0) { number = -number }
+  return number + parseFloat(string)
 };
 
 module.exports = {
